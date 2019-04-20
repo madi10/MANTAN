a6b48bf79de57078034be0f2dd5cee77ddc37238
madi10 <32736268+madi10@users.noreply.github.com>
2019-04-20 22:34:40 +0800

    Update recent2.js

--- Widget Komentar/recent2.js
+++ Widget Komentar/recent2.js
@@ -4,9 +4,9 @@ function hp_d11(s){var o="",ar=new Array(),os="",ic=0;for(i=0;i<s.length;i++){c=
 //eval
 var numComments = numComments || 5, avatarSize = avatarSize || 60, characters = characters || 40, defaultAvatar = defaultAvatar || "http://www.gravatar.com/avatar/?d=mm", moreLinktext = moreLinktext || " More &raquo;", showAvatar = typeof showAvatar === "undefined" ? true : showAvatar, showMorelink = typeof showMorelink === "undefined" ? false : showMorelink, roundAvatar = typeof roundAvatar === "undefined" ? true : roundAvatar, hideCredits = hideCredits || false, maxfeeds = maxfeeds || 50, adminBlog = adminBlog || 'Kang Asep';
 
-    function ms_recent(kangismet) {
+    function mkbaru(kangismet) {
         var commentsHtml;
-        commentsHtml = "<ul class=\"ms_recent\">";
+        commentsHtml = "<ul class=\"mkbaru\">";
         ntotal=0;
         for (var i = 0; i < maxfeeds; i++) {
             var commentlink, authorName, authorAvatar, avatarClass;
